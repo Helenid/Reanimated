0493f96eb73d92921958ec5dde02b5a440ed464a
Idowu Helen <helenidowu490@gmail.com>
2025-07-08 21:28:04 +0100

    updated

--- App.js
+++ App.js
@@ -1,43 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Animated, { useSharedValue, useAnimatedStyle, withTiming, withSpring, withDelay, withRepeat, SharedValue } from 'react-native-reanimated';
+import { StyleSheet, Text, ScrollView } from 'react-native'
+import React from 'react'
+import Animated from 'react-native-reanimated'
+import { Page } from './components/page'
 
-const SIZE = 100.0
+const WORDS = ["What's up", "mobile", "devs"]
 
-const handleRotation = (progress) => {
-  'worklet';
-  return `${progress * 2 * Math.PI}rad`;
-};
-export default function App() {
-  const progress = useSharedValue(1);
-  const scale = useSharedValue(2)
-
-  const reanimatedStyle = useAnimatedStyle (() => {
-    return {
-      opacity:progress.value,
-      borderRadius:(progress.value * SIZE)/2,
-      transform: [{scale:scale.value}, {rotate: handleRotation(progress.value)}]
-    }
-  },[])
-
-  useEffect(()=> {
-    progress.value = withRepeat(withSpring(0.5), 3, true);
-    scale.value= withRepeat(withSpring(1), 3,true);
-  }, [])
-
+const App = () => {
   return (
-    <View style={styles.container}>
-    <Animated.View style={[{ height: SIZE, width: SIZE, backgroundColor: 'purple' }, reanimatedStyle]} />
-    </View>
-  );
+    <Animated.ScrollView horizontal pagingEnabled style={styles.container}>
+     {WORDS.map((title, index) => {
+      return <Page key={index.toString()} title={title} index={index}/>
+     }
+    )}
+    </Animated.ScrollView>
+  )
 }
+
+export default App
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff', 
   },
-});
+})
---
+++ components/Page.js
@@ -0,0 +1,30 @@
+import { View, Text, Dimensions,StyleSheet } from 'react-native'
+import React from 'react'
+
+const { height, width } = Dimensions.get("window")
+const colors = ['blue', 'green', 'purple'];
+
+const Page = ({title, index}) => {
+   return (
+    <View style={[styles.page, { backgroundColor: colors[index % colors.length] }]}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+  
+}
+
+const styles = StyleSheet.create({
+  page: {
+    width,
+    height,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
+
+export { Page }
